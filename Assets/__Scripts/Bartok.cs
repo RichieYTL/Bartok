ad02e6a6931552cf958712ce7b02765d6d649b20
Richie Nguyen <richieytl@gmail.com>
2017-07-17 13:22:21 -0400

    Created the Bartok scene by deleting parts from the Prospector scene. Created CardBartok and Bartok scripts for the maincamera. CardBartok will not build. Line 73 in CardBartok does not work as Gibson wants Quaternion rotQ to be set to Utils.Bezier( uC, bezierRots), which returns a Vector3, not a Quaternion. Gibson explicitly states that the interpolation of Quaternions is important to have the cards in Bartok fan out.

---
+++ Assets/__Scripts/Bartok.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bartok : MonoBehaviour {
+	static public Bartok S;
+	public TextAsset deckXML;
+	public TextAsset layoutXML;
+	public Vector3 layoutCenter = Vector3.zero;
+
+	public Deck deck;
+	public List<CardBartok> drawPile;
+	public List<CardBartok> discardPile;
+	void Awake() {
+		S = this;
+	}
+	void Start () {
+		deck = GetComponent<Deck>();
+		deck.InitDeck(deckXML.text);
+		Deck.Shuffle(ref deck.cards);
+	}
+}
